edfe191864282678f88c5ef323a6d3eaa34626ac
Milan Gadhiya <mgadhiya@my.centennialcollege.ca>
2023-01-13 17:02:43 -0500

    first commit

---
+++ lib.js
@@ -0,0 +1,19 @@
+//division
+export function halfOf(x) {
+    return x / 2;
+    }
+
+//multiplication
+export function multiplication(x, y) {
+    return x * y;
+    }
+
+export function doSomething(name) {
+    console.log('Hey ' + name + '... You did something!!!')
+    };
+
+export let flag = false;
+export function touch() {
+flag = true;
+}
+
---
+++ main.js
@@ -0,0 +1,13 @@
+import { halfOf, multiplication, doSomething, flag, touch  } from "./lib.js";
+
+console.log("Division");
+console.log(halfOf(40));
+
+console.log("Multiplication");
+console.log(multiplication(5, 6));
+
+console.log(doSomething('Milan'));
+
+console.log(flag);
+touch();
+console.log(flag);
